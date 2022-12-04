32456b942722a7e6a73d2f43b42827c6936aa045
norrisb1986 <86497993+norrisb1986@users.noreply.github.com>
2022-12-03 17:38:26 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,29 +1,40 @@
-# Here is the code for Part 1
-with open('input_day_3.txt') as file:
-    lines = file.read().splitlines()
-a = [chr(i) for i in range(97, 97 + 26)]
-a.extend([chr(i) for i in range(65, 65 + 26)])
-ans = 0
-print(lines[:2])
-for line in lines:
-    n = len(line)
-    s, t = line[:n // 2], line[n // 2:]
-    u, v = set(s), set(t)
-    c = u.intersection(v).pop()
-    p = a.index(c) + 1
-    ans += p
-print(ans)
-
-# Here is the code for Part 2
-ans = 0
-n = len(lines)
-for i in range(0, n, 3):
-    s, t, u = lines[i], lines[i + 1], lines[i + 2]
-    x, y, z = set(s), set(t), set(u)
-    c = x.intersection(y).intersection(z).pop()
-    p = a.index(c) + 1
-    ans += p
-print(ans)
+from enum import IntEnum
 
 
+class RPS(IntEnum):
+    Rock = A = X = 1
+    Paper = B = Y = 2
+    Scissors = C = Z = 3
 
+
+rules = {RPS.Rock: RPS.Scissors, RPS.Scissors: RPS.Paper, RPS.Paper: RPS.Rock}
+strategy = {'X': (lambda elf: rules[elf]),
+            'Y': (lambda elf: elf),
+            'Z': (lambda elf: {v: k for k, v in rules.items()}[elf])}
+
+
+def rps_round(player: RPS, elf: RPS) -> int:
+    points = player
+    if rules[player] == elf:
+        points += 6
+    elif player == elf:
+        points += 3
+
+    return points
+
+
+def day2():
+    with open("day2.txt", 'r') as f:
+        rounds = [tuple(x.split(" ")) for x in f.read().splitlines()]
+
+    points1, points2 = 0, 0
+    for e, p in rounds:
+        elf, player, selected_strategy = RPS[e], RPS[p], p
+        points1 += rps_round(player, elf)
+        points2 += rps_round(strategy[selected_strategy](elf), elf)
+
+    print(points1)
+    print(points2)
+
+if __name__ == "__main__":
+    day2()
